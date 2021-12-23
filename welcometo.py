2fb9c09de0330e2c9b1d34aa3d055125616b5721
Andrew Vandever <andrew.vandever@gmail.com>
2021-12-23 10:42:01 -0800

    Add many comments

--- welcometo.py
+++ welcometo.py
@@ -9,21 +9,35 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 @app.route("/<int:seed>", methods=["GET"])
 def game(seed=None):
+    """
+    Return a page representing a game state. We rebuild the state every page
+    load, with the seed ensuring that things turn out the same across loads.
+    If no seed is provided, we set it to the current unix timestamp.
+    """
+    # seed provides continuity for shuffling
     if seed is None:
         seed = int(time.time())
+    # turn tracks advancement through the game
     turn = int(request.args.get("turn", 1))
     next_page = f"/{seed}?turn={turn + 1}"
+    # Set advanced=off in url parameters to exclude advanced plans
     advanced = request.args.get("advanced")
     objs_a = list(range(6 if advanced == "off" else 11))
     objs_b = list(range(6 if advanced == "off" else 11))
     objs_c = list(range(6))
     r = Random(seed)
+    # If this happened after deck shuffling we could end up swapping out plans
     r.shuffle(objs_a)
     r.shuffle(objs_b)
     r.shuffle(objs_c)
     objectives = [objs_a[0], objs_b[0], objs_c[0]]
+    # This is simple enough
     deck = get_deck()
     r.shuffle(deck)
+    # TODO: Each pile should be reshuffled individually
+    # TODO: Implement first goal completed reshuffle
+    # This logic simulates flipping the 27th card from each pile over when
+    # reshuffling
     for x in range(turn // 26):
         extension = deck[(x - 1) * 27 * 3:(27 * x - 1) * 3]
         r.shuffle(extension)
@@ -31,33 +45,48 @@ def game(seed=None):
     piles = [[], [], []]
     for (i, card) in enumerate(deck):
         piles[i % 3].append(card)
+    # Here we actually identify the active cards from each pile
     symbol_cards = [p[turn - 1] for p in piles]
     number_cards = [p[turn] for p in piles]
-    value = "<table><tr>"
     obj_tags = []
+    # We identify the plans here, but don't place them in the html yet because
+    # they will be displayed in a single column, not row
+    # TODO: Support flipping plans to back side
+    # TODO: Parameterize image height and width
     for (i, choice) in enumerate(objectives):
         img = f"/static/card_images/n{i + 1}_{ascii_lowercase[choice]}.png"
         tag = f"<img src=\"{img}\" alt=\"An Objective\" height=272 width=208>"
         obj_tags.append(tag)
-    value += "</tr><tr>"
+    # TODO: Use templates to build page
+    # Building the table as we go is pretty ugly
+    value = "<table><tr>"
+    # Numbers go on top
     for card in number_cards:
         number_img = f"/static/card_images/{card.number_face}"
         tag = f"<img src=\"{number_img}\" alt=\"{card.number} flips to {card.symbol}\" height=272 width=208>"
         value += f"<td>{tag}</td>"
+    # Insert objective n1 at the end of the row
     value += f"<td>{obj_tags[0]}</td>"
+    # Start a new row
     value += "</tr><tr>"
+    # Symbols go in row 2
     for card in symbol_cards:
         symbol_img = f"/static/card_images/{card.symbol_face}"
         tag = f"<img src=\"{symbol_img}\" alt=\"{card.symbol}\" height=272 width=208>"
         value += f"<td>{tag}</td>"
+    # Insert objective n2 at the end of the row
     value += f"<td>{obj_tags[1]}</td>"
+    # Third row consists of link to the next page, and third objective - still
+    # in column 4
     value += f"</tr><tr><td><a href={next_page}>Next Turn</a></td><td></td><td></td>"
     value += f"<td>{obj_tags[2]}</td>"
+    # Wrap up the table
     value += "</tr></table>"
     return value
 
 
 class Card:
+    """Representation of a card"""
     __slots__ = ["number", "symbol"]
     def __init__(self, number: str, symbol: str):
         self.number = number
@@ -65,13 +94,16 @@ class Card:
 
     @property
     def number_face(self):
+        """Construct file name for image of card's number side, like 11_pool.png"""
         return f"{self.number}_{self.symbol}.png"
 
     @property
     def symbol_face(self):
+        """Construct file name for image of card's symol side, like pool.png"""
         return f"{self.symbol}.jpg"
 
 
+# The deck as designed in the original game
 deck = [
     Card(number="one", symbol="investment"),
     Card(number="two", symbol="investment"),
@@ -157,4 +189,5 @@ deck = [
 ]
 
 def get_deck():
+    """Get the deck. This is mostly silly."""
     return deck.copy()
